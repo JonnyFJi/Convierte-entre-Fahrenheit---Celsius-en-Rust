0d272d36337dd5240d9bf0d3387706adb7d3a4ba
Jonny Ji <157997680+JonnyFJi@users.noreply.github.com>
2026-01-15 22:14:27 -0400

    Add files via upload

---
+++ target/release/convierte_fahrenheit_celsius.d
@@ -0,0 +1,1 @@
+/home/jonnyji/projects/convierte_fahrenheit_celsius/target/release/convierte_fahrenheit_celsius: /home/jonnyji/projects/convierte_fahrenheit_celsius/src/main.rs
---
+++ target/release/deps/convierte_fahrenheit_celsius-fd36821ddaf4e27f.d
@@ -0,0 +1,5 @@
+/home/jonnyji/projects/convierte_fahrenheit_celsius/target/release/deps/convierte_fahrenheit_celsius-fd36821ddaf4e27f.d: src/main.rs
+
+/home/jonnyji/projects/convierte_fahrenheit_celsius/target/release/deps/convierte_fahrenheit_celsius-fd36821ddaf4e27f: src/main.rs
+
+src/main.rs:
